223a7bac628c27a3e130d0987189387f96d828eb
celosc <marcelo.conterato@hotmail.com>
2014-09-19 14:50:28 -0300

    Metodo port

--- Energy/energy.py
+++ Energy/energy.py
@@ -1,6 +1,6 @@
 class topology (object):
     
-    def seachpath (self):
+    def searchpath (self):
         pass 
 
 class switch (object):
@@ -12,4 +12,16 @@ class host (object):
 
 
 class port (object):
-    pass
+    
+    def port(self, src, dst):
+        '''Get port number.
+
+        @param src source switch name
+        @param dst destination switch name
+        @return tuple (src_port, dst_port):
+            src_port: port on source switch leading to the destination switch
+            dst_port: port on destination switch leading to the source switch
+        '''
+        if src in self.ports and dst in self.ports[src]:
+            assert dst in self.ports and src in self.ports[dst]
+            return self.ports[src][dst], self.ports[dst][src]
